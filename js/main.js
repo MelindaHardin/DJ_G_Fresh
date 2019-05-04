5fb72c6154108f9f0456d948b19515b28032cb88
Melinda H <melinda.yw.hardin@gmail.com>
2019-05-03 23:25:49 -0500

    one page

--- js/main.js
+++ js/main.js
@@ -12,3 +12,8 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block"; 
   setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
+
+// CONTACT FORM COMPLETED
+
+// TESTIMONIAL FORM COMPLETED
+
