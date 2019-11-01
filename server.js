86c84b621300208f8a32aa1527b08fff87000b63
Melinda H <melinda.yw.hardin@gmail.com>
2019-10-31 21:54:00 -0500

    config

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var express = require ('express');
+var express = require("express");
 var app = express();
-var mongoose = require ("mongoose");
-var bodyParser = require ("body-parser");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
 //var db = require("./models")
-var PORT= 3000;
+var PORT = 3001;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -11,25 +11,25 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 // --------------- CONTACT FORM (mongo) ---------------
-mongoose.connect ("mongodb://localhost:27017/clientDB", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/clientDB", {
+  useNewUrlParser: true
+});
 
-var clientInqFormSchema = new mongoose.Schema ({
-
+var clientInqFormSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   phoneNumber: String,
-  email: String, 
+  email: String,
   eventType: String,
   eventDate: Date,
   eventStart: String,
   eventEnd: String,
   message: String
-})
+});
 
-var Client_Inq = mongoose.model ("Client_Inq", clientInqFormSchema);
+var Client_Inq = mongoose.model("Client_Inq", clientInqFormSchema);
 // --------------- CONTACT FORM (site) ---------------
-app.post("/contact.html", function(req, res){
-
+app.post("/contact.html", function(req, res) {
   var clientInqFirstName = req.body.formFirstName;
   var clientInqLastName = req.body.formLastName;
   var clientInqPhone = req.body.formPhone;
@@ -40,7 +40,7 @@ app.post("/contact.html", function(req, res){
   var clientInqEventEnd = req.body.formEventEnd;
   var clientInqEventMessage = req.body.formMessage;
 
- var client= new Client_Inq ({
+  var client = new Client_Inq({
     firstName: clientInqFirstName,
     lastName: clientInqLastName,
     phoneNumber: clientInqPhone,
@@ -50,47 +50,47 @@ app.post("/contact.html", function(req, res){
     eventStart: clientInqEventStart,
     eventEnd: clientInqEventEnd,
     message: clientInqEventMessage
- })
+  });
 
   client.save();
   res.redirect("/");
   console.log(client);
-})
-
+});
 
 // --------------- TESTIMONIAL FORM (Mongo) ---------------
-var testimonialSubmissionSchema = new mongoose.Schema ({
+var testimonialSubmissionSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true}, 
-  eventType: { type: String, require: true},
-  feeback: { type: String, require: true},
+  email: { type: String, required: true },
+  eventType: { type: String, require: true },
+  feeback: { type: String, require: true }
   //password: { type: String, required: true}
+});
 
-})
-
-var Testimonial_Submission = mongoose.model ("Testimonial", testimonialSubmissionSchema);
+var Testimonial_Submission = mongoose.model(
+  "Testimonial",
+  testimonialSubmissionSchema
+);
 // --------------- TESTIMONIAL FORM (Site) ---------------
-app.post("/", function (req, res){
+app.post("/", function(req, res) {
   var testmonialSubmittedName = req.body.testimonialSubmitterName;
   var testmonialSubmittedEmail = req.body.testimonialSubmitterEmail;
-  var testmonialSubmittedEventType =req.body.testimonialSubmitterEventType;
+  var testmonialSubmittedEventType = req.body.testimonialSubmitterEventType;
   var testmonialSubmittedFeedback = req.body.testimonialSubmitterFeedback;
   //var testmonialSubmittedPassword = testimonialSubmitterPassword;
 
-  var testimonial_submission = new Testimonial_Submission ({
-    name: testmonialSubmittedName, 
-    email: testmonialSubmittedEmail, 
+  var testimonial_submission = new Testimonial_Submission({
+    name: testmonialSubmittedName,
+    email: testmonialSubmittedEmail,
     eventType: testmonialSubmittedEventType,
-    feeback: testmonialSubmittedFeedback, 
+    feeback: testmonialSubmittedFeedback
     //password: testmonialSubmittedPassword
-  })
+  });
   testimonial_submission.save();
   res.redirect("/#testimonials");
   console.log(testimonial_submission);
-})
-
+});
 
 // --------------- SERVER LISTENING ---------------
-app.listen(PORT, function(req, res){
-  console.log ("server listening on port" + PORT );
-})
+app.listen(PORT, function(req, res) {
+  console.log("server listening on port" + PORT);
+});
